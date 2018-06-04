751fbf54c54a01e04b799e8aa8488364603ce17c
Tomoki.Sugai <tomoki.sugai@rakuten.com>
2018-06-04 00:58:29 +0000

    Assignment

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,7 +7,7 @@
 
      {!! Form::model($task, ['route' => 'tasks.store']) !!}
 <div class="row">
-             <div class="col-sm-offset-2 col-sm-5">
+                     <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
                 <div class="form-group">
                     {!! Form::label('status', 'status:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
